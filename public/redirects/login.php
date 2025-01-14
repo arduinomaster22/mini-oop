54e2a355bcec0de410e9fcb65f984f92488afc19
MHortulanus <manoj@vormkracht10.nl>
2025-01-14 18:46:11 +0100

    wip

---
+++ public/redirects/login.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . '/../bootstrap/app.php';
+
+// Login logic 
+
+
+
+// After script run: redirect to another page
+// Redirct: https://blog.hubspot.com/website/php-redirect#:~:text=Lastly%2C%20you%20have%20to%20wrap%20the%20function%20in%20%3C%3Fphp%20and%20%3F%3E%20tags.%20The%20result%20will%20look%20something%20like%20this%3A
